35b9119ebb5e67371da31604e34e8a81c9727fcb
Max <mdiazn@uni.pe>
2023-02-17 12:23:07 -0500

    JournalLayour y JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,11 @@
-import MailOutline from "@mui/icons-material/MailOutline"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant="h1">JournalPage</Typography>
-    <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, labore? Magnam perspiciatis nulla repudiandae, suscipit, quam ullam nemo rerum commodi cumque, praesentium eligendi accusamus voluptatibus sit ducimus. Minus, quas in?</Typography>
+    </JournalLayout>
   )
 }
