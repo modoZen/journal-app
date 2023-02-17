59c738ca2dfc8f9709037eac87c3682f0a2ce288
Max <mdiazn@uni.pe>
2023-02-16 21:29:05 -0500

    instalacion Material MUI

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from "react-router-dom"
 import { AppRouter } from "./router/AppRouter"
+import { AppTheme } from "./theme/AppTheme"
 
 export const JournalApp = () => {
   return (
-    <BrowserRouter>
+    <AppTheme>
       <AppRouter />
-    </BrowserRouter>
+    </AppTheme>
   )
 }
