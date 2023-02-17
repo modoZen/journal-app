dd4ef6d3c266a3d83788eba698157a90d5aad9f7
Max <mdiazn@uni.pe>
2023-02-17 15:46:03 -0500

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,14 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, labore? Magnam perspiciatis nulla repudiandae, suscipit, quam ullam nemo rerum commodi cumque, praesentium eligendi accusamus voluptatibus sit ducimus. Minus, quas in?</Typography>
+      {/* <Typography>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, labore? Magnam perspiciatis nulla repudiandae, suscipit, quam ullam nemo rerum commodi cumque, praesentium eligendi accusamus voluptatibus sit ducimus. Minus, quas in?</Typography> */}
+
+      <NothingSelectedView />
+      {/* NoteView */}
     </JournalLayout>
   )
 }
