46f970f0d7e9ce7f9dd0e295978fa5c27ee81831
Max <mdiazn@uni.pe>
2023-02-19 11:35:23 -0500

    Buton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -10,6 +12,23 @@ export const JournalPage = () => {
 
       {/* <NothingSelectedView /> */}
       <NoteView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': {
+            backgroundColor: 'error.main',
+            opacity: 0.9,
+          },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
